c9b755cc7b608b9fb5223adf486468d8edacd14f
Serkan Hosca <serkan@hosca.com>
2021-07-14 11:16:59 -0400

    telescope

--- vim/.config/nvim/lua/telescope.lua
+++ vim/.config/nvim/lua/telescope.lua
@@ -1,9 +1,15 @@
 local M = {}
 
+local function config()
+  vim.api.nvim_set_keymap("n", "<Leader>f", ":Telescope find_files<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<Leader>b", ":Telescope buffers<CR>", { noremap = true, silent = true })
+end
+
 function M.configure_packer(use)
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+    config = config,
   }
 end
 return M
