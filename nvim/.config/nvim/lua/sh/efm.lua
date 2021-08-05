dd6a7c3ee6989f36b9ff03873a5b97a6a097c203
Serkan Hosca <serkan@hosca.com>
2021-08-05 09:28:17 -0400

    Remove efm configs python

--- nvim/.config/nvim/lua/sh/efm.lua
+++ nvim/.config/nvim/lua/sh/efm.lua
@@ -4,30 +4,6 @@ local lsp = require('sh.lsp')
 
 local M = {}
 
-function M.get_python_setup()
-    local venv = require('sh.utils').get_python_venv()
-    local bin_path = ''
-    if venv then bin_path = lsputil.path.join(venv, 'bin') .. '/' end
-
-    local root_markers = {".git", "poetry.lock", "pyproject.toml", "Pipfile", "requirements.txt", "requirements.in", "setup.cfg", "setup.py"}
-
-    return {
-        {
-            lintCommand = bin_path .. "flake8 --stdin-display-name '${INPUT}' -",
-            lintStdin = true,
-            lintIgnoreExitCode = true,
-            lintFormats = {'%f:%l:%c: %m', '%f = %l:%c: %m'},
-            rootMarkers = root_markers
-        }, {formatCommand = bin_path .. 'black --quiet -', formatStdin = true, rootMarkers = root_markers, formatIgnoreExitCode = true}
-        -- {
-        --     lintCommand = bin_path .. 'mypy --show-column-numbers',
-        --     lintStdin = false,
-        --     lintFormats = {'%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m', '%f:%l:%c: %tote: %m'},
-        --     rootMarkers = root_markers
-        -- }
-    }
-end
-
 function M.lua_setup() return {{formatCommand = 'lua-format --column-limit 150 -i', formatStdin = true, rootMarkers = {".git"}}} end
 
 function M.setup()
@@ -37,7 +13,7 @@ function M.setup()
         capabilities = lsp.capabilities(),
         init_options = {documentFormatting = true, documentSymbol = true, codeAction = true, hover = true},
         filetypes = {"python", "lua"},
-        settings = {languages = {python = M.get_python_setup(), lua = M.lua_setup()}}
+        settings = {languages = {lua = M.lua_setup()}}
     }
 
 end
