7fda3839a864740cd56fa03da155941d33f3cebd
Serkan Hosca <serkan@hosca.com>
2021-12-09 09:20:21 -0500

    qf

--- nvim/.config/nvim/lua/sh/core.lua
+++ nvim/.config/nvim/lua/sh/core.lua
@@ -68,10 +68,10 @@ function M.configure_packer(use)
       local map = vim.keymap.map
       local vmap = vim.keymap.vmap
       vim.g.slimux_buffer_filetype = 'slimux'
-      map {"<Leader>s", ":SlimuxREPLSendLine<CR>"}
-      vmap {"<Leader>s ", ":SlimuxREPLSendSelection<CR>"}
-      map {"<Leader>b", ":SlimuxREPLSendBuffer<CR>"}
-      map {"<Leader>k", ":SlimuxSendKeysLast<CR>"}
+      map {"<leader>s", ":SlimuxREPLSendLine<CR>"}
+      vmap {"<leader>s ", ":SlimuxREPLSendSelection<CR>"}
+      map {"<leader>b", ":SlimuxREPLSendBuffer<CR>"}
+      map {"<C-l><C-l>", ":SlimuxSendKeysLast<CR>"}
       map {"<C-c><C-c>", ":SlimuxREPLSendLine<CR>"}
       vmap {"<C-c><C-c>", ":SlimuxREPLSendSelection<CR>"}
     end
@@ -81,15 +81,29 @@ function M.configure_packer(use)
     config = function()
       local nnoremap = vim.keymap.nnoremap
       require("harpoon").setup {projects = {}}
-      nnoremap {"<Leader>a", function() require("harpoon.mark").add_file() end}
+      nnoremap {"<leader>a", function() require("harpoon.mark").add_file() end}
       nnoremap {"<C-e>", function() require("harpoon.ui").toggle_quick_menu() end}
-      nnoremap {"<Leader>tc", function() require("harpoon.cmd-ui").toggle_quick_menu() end}
+      nnoremap {"<leader>tc", function() require("harpoon.cmd-ui").toggle_quick_menu() end}
       nnoremap {"<C-h>", function() require("harpoon.ui").nav_file(1) end}
       nnoremap {"<C-t>", function() require("harpoon.ui").nav_file(2) end}
       nnoremap {"<C-n>", function() require("harpoon.ui").nav_file(3) end}
       nnoremap {"<C-s>", function() require("harpoon.ui").nav_file(4) end}
     end
   }
-  use {"ten3roberts/qf.nvim", config = function() require('qf').setup {} end}
+  use {
+    "ten3roberts/qf.nvim",
+    config = function()
+      local nnoremap = vim.keymap.nnoremap
+      require('qf').setup {}
+      nnoremap {"<leader>ll", function() require("qf").toggle('l', true) end}
+      nnoremap {"<leader>cc", function() require("qf").toggle('c', true) end}
+      nnoremap {"<leader>j", function() require('qf').below('l') end}
+      nnoremap {"<leader>k", function() require('qf').above('l') end}
+      nnoremap {"<leader>J", function() require'qf'.below('c') end}
+      nnoremap {"<leader>K", function() require'qf'.above('c') end}
+      nnoremap {"]q", function() require('qf').below('visible') end}
+      nnoremap {"[q", function() require'qf'.above('visible') end}
+    end
+  }
 end
 return M
