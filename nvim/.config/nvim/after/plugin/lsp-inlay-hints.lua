7be849c63ddb41f32964ead7387fc6d31babce9d
Serkan Hosca <serkan@hosca.com>
2024-02-16 14:54:57 -0500

    Updates

--- nvim/.config/nvim/after/plugin/lsp-inlay-hints.lua
+++
@@ -1,6 +0,0 @@
-require("sh.utils").lsp_attach(function(client, bufnr)
-  local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
-  if inlay_hint and client.supports_method("textDocument/inlayHint") then
-    inlay_hint(bufnr, true)
-  end
-end)
